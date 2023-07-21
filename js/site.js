a3e573f68bd3a913983699ba18a34ff7bddb376a
Rellium <relliumachine42@gmail.com>
2023-07-21 14:32:07 -0400

    count 100 with description and safeguards

--- js/site.js
+++ js/site.js
@@ -1,13 +1,78 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    
-    let message = inputBox.value;
+// Gets numbers from our form inputs
+// ENTRY POINT
+// Controller function
+function getValues() {
+    let startNumber = document.getElementById('startValue').value;
+    let endNumber = document.getElementById('endValue').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
+    startNumber = Number(startNumber);
+    endNumber = Number(endNumber);
+
+    if(isNaN(startNumber) == true || isNaN(endNumber) == true) {
+        Swal.fire({
+            title: 'Woopsie!',
+            text: 'Count 100 only works with numbers.',
+            icon: 'error',
+            backdrop: false
+        });
+    } else if(startNumber > endNumber) {
+            Swal.fire({
+                title: 'Oops!',
+                text: 'The starting number must be less than the ending number',
+                icon: 'error',
+                backdrop: false
+            });
+            
+        } else {
+            let numberArray = generateNumbers(startNumber, endNumber);
+        
+            displayNumbers(numberArray);
         }
-    );
+
+    }
+
+}
+
+// Business logic- creates every number in the input range
+// Data Model
+function generateNumbers(start, end) {
+
+    let range = [];
+
+    for(let number = start; number <= end; number++) {
+        
+        range.push(number);
+
+
+    }
+
+    return range;
+
+}
+
+// puts the numbers on the page
+// View Function
+function displayNumbers(numbersToDisplay) {
+
+    let tableHtml = '';
+    
+    for(let index = 0; index < numbersToDisplay.length; index++) {
+
+        let currentNumber = numbersToDisplay[index];
+
+        let className = '';
+
+        if(currentNumber % 2 == 0) {
+            className = 'even';
+        } else {
+            className = 'odd';
+        }
+
+        let tableRowHtml = `<tr><td class="${className}">${currentNumber}</td></tr>`;
+
+        tableHtml += tableRowHtml;
+    }
+
+    document.getElementById('results').innerHTML = tableHtml;
+
 }
